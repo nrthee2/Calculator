4a20750bb9da0403d4c4c66bba6122c7db14f884
Olivér Mészáros <procod@hotmail.com>
2022-03-09 19:37:43 +0100

    JS is done for now. CSS isn't. Known problems: pressing equal multiple times and pressing only operators aren't working the same way as on real calculators.

--- script.js
+++ script.js
@@ -7,10 +7,21 @@ let multiply = (a, b) => a*b;
 
 let divide = (a, b) => {
     if (b === 0){
-        return "Division by zero is undefined!"
+        return display.textContent = "Can't divide by zero";
     } 
     return a/b;
 };
+
+//top 2 buttons
+const btnfirst = document.getElementById("first");
+btnfirst.addEventListener("click", () => {
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+});
+
+const btnsecond = document.getElementById("second");
+btnsecond.addEventListener("click", () => {
+    alert(":)");
+});
 
 //operate, calling an operator and two numbers, returning one of the basic operations
 let operate = (operator, a, b) => {
@@ -43,7 +54,13 @@ let displayHold;
 const btnnumber = document.querySelectorAll("button.number");
 btnnumber.forEach((button) => {
     button.addEventListener("click", () => {
+        if (countdisplay === 0) {
+            display.textContent = "";
             display.textContent = Number(display.textContent + button.textContent);
+        } else if (countdisplay >= 1) {
+            display.textContent = Number(display.textContent + button.textContent);
+        }
+        ++countdisplay;
     });
 });
 
@@ -51,6 +68,8 @@ btnnumber.forEach((button) => {
 const btnoperator = document.querySelectorAll("button.operator");
 btnoperator.forEach((button) => {
     button.addEventListener("click", () => {
+        countdisplay = 0;
+        countdecimal = 0;
         if (count === 0) {
             prevValue = Number(display.textContent);
                 displayHold = (displaySecondary.textContent = display.textContent + " " + button.textContent + " ");
@@ -93,6 +112,7 @@ const btnclear = document.querySelector("#clear");
 btnclear.addEventListener("click", () => {
     count = 0;
     countdecimal = 0;
+    countdisplay = 0;
     prevValue = 0;
     nextValue = 0;
     currValue = 0;
@@ -125,9 +145,45 @@ btnequals.addEventListener("click", () => {
     prevValue = currValue;
     display.textContent = currValue;
     displaySecondary.textContent = displayHold + nextValue + " =";
-    console.log("prev: " + prevValue + " next: " + nextValue + " finally curr: " + currValue);
 });
 
-/*Things to do:
-    Fix the display: the calculated expression should be in the display, 
-    and when you start typing again, it should replace it, not concatenate to the existing value*/
+//keyboard support
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const operators = ["+", "-", "x", "/", "=", "Backspace", "Delete"];
+let operatorKey;
+document.addEventListener("keydown", (event) =>{
+    let num = event.key;
+    if (numbers.includes(Number(num))) {
+        if (countdisplay === 0) {
+            display.textContent = "";
+            display.textContent = Number(display.textContent + num);
+        } else if (countdisplay >= 1) {
+            display.textContent = Number(display.textContent + num);
+        }
+        ++countdisplay;
+    }
+    if (operators.includes(num)) {
+        if (num === "+") {
+            operatorKey = "add";
+            document.getElementById(`${operatorKey}`).click();
+        } else if (num === "-") {
+            operatorKey = "subtract";
+            document.getElementById(`${operatorKey}`).click();
+        } else if (num === "x") {
+            operatorKey = "multiply";
+            document.getElementById(`${operatorKey}`).click();
+        } else if (num === "/") {
+            operatorKey = "divide";
+            document.getElementById(`${operatorKey}`).click();
+        } else if (num === "=") {
+            operatorKey = "equals";
+            document.getElementById(`${operatorKey}`).click();
+        } else if (num === "Backspace") {
+            operatorKey = "delete";
+            document.getElementById(`${operatorKey}`).click();
+        } else if (num === "Delete") {
+            operatorKey = "clear";
+            document.getElementById(`${operatorKey}`).click();
+        }
+    };
+});
